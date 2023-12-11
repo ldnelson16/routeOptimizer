643a5b0c63aee4e5f9e68773797bedcffafe047e
ldnelson16 <ldnelson16@gmail.com>
2023-12-11 01:00:20 -0500

    reorganization

--- exceptions.cpp
+++
@@ -1,6 +0,0 @@
-#include "Exceptions.hpp"
-#include "Map.hpp"
-
-LocationAlreadyExists::LocationAlreadyExists(Location loc) {
-  errorMessage = "Location "+loc.name+" at (Lat,Lon) ("+to_string(round(loc.latitude*100000)/100000)+","+to_string(round(loc.longitude*100000)/100000)+")";
-}
