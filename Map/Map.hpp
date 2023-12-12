68187aa691ef54b6a37f710fb61c1449c7594301
ldnelson16 <ldnelson16@gmail.com>
2023-12-12 04:10:00 -0500

    Map abstracts private exceptions

--- Map/Map.hpp
+++ Map/Map.hpp
@@ -10,7 +10,6 @@
 
 #include "Location.hpp"
 #include "Path.hpp"
-#include "../ExceptionsErrors/Exceptions.hpp"
 
 using namespace std;
 
@@ -30,6 +29,49 @@ class Map {
   private:
     vector<Location> nodes;
   public:
+    friend class LocationDoesntExist;
+    friend class PathAlreadyExists;
+    friend class LocationAlreadyExists;
+    // EXCEPTIONS (PRIVATE TO MAP)
+    class LocationAlreadyExists: public std::exception {
+    private: 
+      string errorMessage;
+    public:
+      LocationAlreadyExists(const Location &loc) {
+        errorMessage = "Location already exists.\n"+loc.str();
+      }
+      
+      const char* what() const noexcept override {
+        return errorMessage.c_str();
+      }
+    };
+    class PathAlreadyExists: public std::exception {
+      private: 
+        string errorMessage;
+      public:
+        PathAlreadyExists(const Path &p) {
+          errorMessage = "Path already exists.\n"+p.str();
+        } 
+        const char* what() const noexcept override {
+          return errorMessage.c_str();
+        }
+    };
+    class LocationDoesntExist: public std::exception {
+      private: 
+        string errorMessage;
+      public:
+        LocationDoesntExist(const vector<Location>& locs) {
+          errorMessage = "The following location(s) aren't in the map yet:\n";
+          ostringstream message;
+          for (const Location& loc: locs) {
+            message << loc.str();
+          }
+          errorMessage+=message.str();
+        } 
+        const char* what() const noexcept override {
+          return errorMessage.c_str();
+        }
+    };
     // Default constructor (takes no inputs)
     Map() {}
 
@@ -44,33 +86,62 @@ class Map {
 
     // Add Path btwn two Location nodes
     void addPath(const Location &loc_a, const Location &loc_b, TerrainType terrain, string name) {
-      /*for (Path path: loc_a.paths) {
-        if (path==Path(name,terrain,loc_a,loc_b)) {throw PathAlreadyExists(Path(name,terrain,loc_a,loc_b));}
-      }*/
       auto ita = lower_bound(nodes.begin(),nodes.end(),loc_a);
       auto itb = lower_bound(nodes.begin(),nodes.end(),loc_b);
       // both locations exist
       if (ita!=nodes.end() && itb!=nodes.end() && *ita==loc_a && *itb==loc_b) {
         // path already exists
-        for (auto it: ita->paths) {
+        for (auto &it: ita->paths) {
           if (it==Path(name,terrain,loc_a,loc_b)) {
             throw PathAlreadyExists(Path(name,terrain,loc_a,loc_b));
           }
         }
+        //path doesnt exist
         (*ita).paths.push_back(Path(name,terrain,loc_a,loc_b));
         (*itb).paths.push_back(Path(name,terrain,loc_b,loc_a));
-        //path doesnt exist
       }
       // one or neither locations exist
       else {
-        throw LocationDoesntExist(loc_a, loc_b);
+        if ((ita==nodes.end()||*ita!=loc_a)&&(itb==nodes.end()||*ita!=loc_b)) {
+          throw LocationDoesntExist({loc_a,loc_b});
+        }
+        else if (ita==nodes.end()||*ita!=loc_a) {
+          throw LocationDoesntExist({loc_a});
+        }
+        else {
+          throw LocationDoesntExist({loc_b});
+        }
       }
     }
 
     // Add Path btwn two Location nodes
-    void addPath(Path &path) {
-      (path.startendpair.first->paths).push_back(path);
-      (path.startendpair.second->paths).push_back(path);
+    void addPath(const Path &path) {
+      auto ita = lower_bound(nodes.begin(),nodes.end(),*(path.startendpair.first));
+      auto itb = lower_bound(nodes.begin(),nodes.end(),*(path.startendpair.second));
+      // both locations exist
+      if (ita!=nodes.end() && itb!=nodes.end() && *ita==*(path.startendpair.first) && *itb==*(path.startendpair.second)) {
+        // path already exists
+        for (auto &it: ita->paths) {
+          if (it==path) {
+            throw PathAlreadyExists(path);
+          }
+        }
+        //path doesnt exist
+        (*ita).paths.push_back(path);
+        (*itb).paths.push_back(Path(path));
+      }
+      // one or neither locations exist
+      else {
+        if ((ita==nodes.end()||*ita!=*(path.startendpair.first))&&(itb==nodes.end()||*ita!=*(path.startendpair.second))) {
+          throw LocationDoesntExist({*(path.startendpair.first),*(path.startendpair.second)});
+        }
+        else if (ita==nodes.end()||*ita!=*(path.startendpair.first)) {
+          throw LocationDoesntExist({*(path.startendpair.first)});
+        }
+        else {
+          throw LocationDoesntExist({*(path.startendpair.second)});
+        }
+      }
     }
 };
 
