6f1ed98bebc59e2d3a3733789ee0d4ce59ffd5b0
ldnelson16 <ldnelson16@gmail.com>
2023-12-12 11:15:33 -0500

    TerrainType abstracted

---
+++ Map/TerrainType.hpp
@@ -0,0 +1,12 @@
+#ifndef TERRAINTYPE_HPP
+#define TERRAINTYPE_HPP
+
+// Variable to represent type of terrain
+enum TerrainType { 
+  Paved,
+  Gravel,
+  Dirt,
+  Grass
+};
+
+#endif // TERRAINTYPE_HPP
