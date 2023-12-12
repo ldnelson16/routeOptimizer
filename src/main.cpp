b399718fa93b5f98d89920cf4d6fcb14e33a33c4
ldnelson16 <ldnelson16@gmail.com>
2023-12-12 04:10:30 -0500

    Slight change

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,6 @@ int main() {
   map.addLocation(Location("Bye",100.2,100.2));
   map.addLocation(Location("G",0.5,0.6));
   map.addPath(Location("h",100.1,100.2),Location("e",100.2,100.2),Paved,"name2");
-  map.addPath(Location("Green grass",0.5,0.6),Location("Blue sky",1.5,0.6),Gravel,"name");
+  map.addPath(Location("Green grass",0.5,0.6),Location("Blue sky",100.1,100.2),Gravel,"name");
   cout << "Done";
 }
