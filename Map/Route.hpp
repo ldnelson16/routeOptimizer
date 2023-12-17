e86b81983e412acf891bcde9c6dbf9e55ec571cc
ldnelson16 <ldnelson16@gmail.com>
2023-12-16 21:11:44 -0500

    data type to store Routes

--- Map/Route.hpp
+++ Map/Route.hpp
@@ -2,6 +2,7 @@
 #define ROUTE_HPP
 
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -12,11 +13,32 @@ class Location;
 class Route {
   friend class GeoMap;
   friend class Location;
+  friend class CompareLocations;
   private: 
     vector<Path*> paths;
+    double distance;
   public: 
-    Route (vector<Path*> paths_in): paths(paths_in) {}
+    Route (vector<Path*> paths_in);
     bool inRoute(Location* loc) const;
+    bool operator<(const Route& other) {
+      if (distance==other.distance) {return paths.size() < other.paths.size();}
+      else {return distance < other.distance;}
+    }
+    void operator+=(const Route& other) {
+      paths.insert(paths.end(),other.paths.begin(),other.paths.end());
+      distance += other.distance;
+    }
+    bool contains(Location* loc) const;
+};
+
+class NoRouteExists: public std::exception {
+  private:
+    string errorMessage;
+  public:
+    NoRouteExists(Location* start, Location* end);
+    const char* what() const noexcept override {
+      return errorMessage.c_str();
+    }
 };
 
 #endif // ROUTE_HPP
--- Map/route.cpp
+++ Map/route.cpp
@@ -10,3 +10,23 @@ bool Route::inRoute(Location* loc) const {
   }
   return false;
 }
+
+NoRouteExists::NoRouteExists(Location* start, Location* end) {
+  errorMessage = "No path from " + start->str(true) + " to " + end->str(true) + ".\n";
+}
+
+Route::Route (vector<Path*> paths_in): paths(paths_in) {
+  distance = 0;
+  for (auto path_ptr: paths_in) {
+    distance+=path_ptr->distance;
+  }
+}
+
+bool Route::contains(Location* loc) const {
+  for (auto path_ptr: paths) {
+    if (path_ptr->dest==loc || path_ptr->start==loc) {
+      return true;
+    }
+  }
+  return false;
+}
